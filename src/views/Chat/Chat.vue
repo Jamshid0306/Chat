18545176f405499f7d49e640f584beb0286ba1ad
Abdukarimov Jamshid <abdukarimovjamshid@MacBook-Pro-Abdukarimov.local>
2025-08-05 15:44:53 +0500

    add locale

--- src/views/Chat/Chat.vue
+++ src/views/Chat/Chat.vue
@@ -1,18 +1,22 @@
 <script setup>
-import UserIcon from "../../components/icons/UserIcon.vue";
-import SendIcon from "../../components/icons/SendIcon.vue";
-import "./chat.scss";
+import { useI18n } from "vue-i18n"
+import UserIcon from "../../components/icons/UserIcon.vue"
+import SendIcon from "../../components/icons/SendIcon.vue"
+import "./chat.scss"
+
+const { t } = useI18n()
 </script>
+
 <template>
   <section class="chat">
     <div class="chat-block">
       <div class="chat-block-top">
         <UserIcon :size="35" />
-        <span>Reception</span>
+        <span>{{ t('chat.reception') }}</span>
       </div>
       <div class="chat-block-main"></div>
       <div class="chat-block-input">
-        <input type="text" placeholder="Send Message" />
+        <input type="text" :placeholder="t('chat.sendMessage')" />
         <SendIcon class="sendIcon" :size="35" :color="'rgb(0, 162, 255)'" />
       </div>
     </div>
